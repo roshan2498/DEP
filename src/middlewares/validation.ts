a53f316e9bdee1f5577ef9340fcfc1dce2ff5ad8
roshan2498 <roshan_bt2k16@dtu.ac.in>
2022-02-01 14:26:35 +0530

    cleanup

--- src/middlewares/validation.ts
+++
@@ -1,9 +0,0 @@
-import { schema } from "../services/schema_validation";
-import { Request, Response, NextFunction } from "express";
-
-export const validation = (User: { rawAttributes: {} }) => {
-  return (req: Request, res: Response, next: NextFunction): void => {
-    let nextStep = true;
-    const userColumns = Object.keys(User.rawAttributes);
-  };
-};
